d870f437196f991ff4a2247ee2f9a4f530067d55
Steven Than <thuyen.than@appfolio.com>
2020-05-28 12:34:02 -0400

    newlines at the end of files

--- app/javascript/App.tsx
+++ app/javascript/App.tsx
@@ -1,9 +1,9 @@
 import React, { Fragment } from "react";
 
 export default function App() {
-    return (
-        <Fragment>
-            <h1>Hello test</h1>
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <h1>Hello test</h1>
+    </Fragment>
+  );
 }
