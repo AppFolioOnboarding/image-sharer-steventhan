c54a6f836b605a510504bec60880e1ff2e8530bd
Steven Than <thuyen.than@appfolio.com>
2020-06-01 15:34:50 -0400

    updated ImageLinkForm to handle invalid link, and displayed proper error message from frontend as well as backend

---
+++ app/javascript/components/ImageLinkForm.spec.tsx
@@ -0,0 +1,117 @@
+import React from "react";
+import { mount, shallow, ReactWrapper } from "enzyme";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import axios from "axios";
+import { act } from "react-dom/test-utils";
+import { StaticRouter } from "react-router-dom";
+
+import * as hooks from "../hooks";
+import ImageLinkForm from "./ImageLinkForm";
+
+jest.mock("axios");
+const axiosMock = axios as jest.Mocked<typeof axios>;
+const mockHistoryPush = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
+
+test("renders without crashing", () => {
+  shallow(<ImageLinkForm />);
+});
+
+describe("Form", () => {
+  let wrapper: ReactWrapper;
+  let uploadButton: ReactWrapper;
+
+  beforeAll(() => {
+    wrapper = mount(
+      <StaticRouter location="/view/100">
+        <ImageLinkForm />
+      </StaticRouter>
+    );
+    uploadButton = wrapper.find('button');
+  })
+
+  it("should contains required elements", () => {
+    const input = wrapper.find('input');
+    expect(wrapper.find('form').length).toBe(1);
+    expect(input.length).toBe(1);
+    expect(uploadButton.length).toBe(1);
+  });
+
+  it("should update input as user types", () => {
+    const input = wrapper.find('input');
+    const useStateWithCheckSpy = jest.spyOn(hooks, "useStateWithCheck");
+    const value = "hello";
+    input.simulate("change", { target: { value } });
+    expect(useStateWithCheckSpy).toBeCalledTimes(1);
+    expect(useStateWithCheckSpy).toHaveBeenCalledWith(
+      expect.any(String), expect.any(Function));
+    wrapper.update();
+    expect(wrapper.find('input').length).toBe(1);
+    expect(wrapper.find('input').props().value).toBe(value);
+  });
+
+  it("should show error and disable submit button if input is not valid", () => {
+    const input = wrapper.find('input');
+    const value = "an invalid link";
+    input.simulate("change", { target: { value } });
+    wrapper.update();
+
+    const helperText = wrapper.find(FormHelperText);
+    expect(helperText.length).toBe(1);
+    expect(helperText.find('p').text()).toBe("Invalid link format");
+
+    const uploadButton = wrapper.find('button');
+    expect(uploadButton.props().disabled).toBe(true);
+  });
+
+  it("should hide error element and enable submit button if input is valid", () => {
+    const input = wrapper.find('input');
+    const value = "https://google.com";
+    input.simulate("change", { target: { value } });
+    wrapper.update();
+    expect(wrapper.find(FormHelperText).length).toBe(0);
+    const uploadButton = wrapper.find('button');
+    expect(uploadButton.props().disabled).toBe(false);
+  });
+
+  it("should show error element if submission returns error", async () => {
+    const message = "Something went wrong";
+    axiosMock.post.mockRejectedValueOnce({
+      response: { data: { message } }
+    });
+    const input = wrapper.find('input');
+    const value = "https://google.com";
+    input.simulate("change", { target: { value } });
+    wrapper.update();
+    const form = wrapper.find('form');
+    await act(async () => {
+      form.simulate("submit");
+    })
+    wrapper.update();
+    expect(axiosMock.post).toBeCalledTimes(1);
+    const helperText = wrapper.find(FormHelperText);
+    expect(helperText.length).toBe(1);
+    expect(helperText.find('p').text()).toBe(message);
+  });
+
+  it("should redirect to <ImageView /> submission goes thru", async () => {
+    const location = "/view/16"
+    axiosMock.post.mockResolvedValueOnce({
+      data: { location }
+    });
+    const input = wrapper.find('input');
+    const value = "https://google.com";
+    input.simulate("change", { target: { value } });
+    wrapper.update();
+    const form = wrapper.find('form');
+    await act(async () => {
+      form.simulate("submit");
+    });
+    expect(mockHistoryPush).toHaveBeenCalledWith(location);
+  });
+})
--- app/javascript/components/ImageLinkForm.tsx
+++ app/javascript/components/ImageLinkForm.tsx
@@ -1,30 +1,81 @@
-import React, { FC, useState, ChangeEvent, MouseEvent, FormEvent } from "react";
+import React, { FormEvent } from "react";
+import Button from '@material-ui/core/Button';
+import axios from "axios";
+import { TextField, Box, Grid, Container } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
-export default function App(): JSX.Element {
-  const [link, setLink] = useState("");
+import { useStateWithCheck, useAsyncReducer, ActionType } from "../hooks";
+import { IMG_ENDPOINT } from "../constants";
 
-  function handleFormSubmit(fe: FormEvent) {
+
+interface ResponseData {
+  id: number;
+  location: string;
+}
+
+export default function ImageLinkForm(): JSX.Element {
+  const [link, linkError, setLink] = useStateWithCheck("", link => {
+    try {
+      new URL(link);
+      return null;
+    } catch(e) {
+      return new Error("Invalid link format")
+    }
+  });
+  const [state, dispatch] = useAsyncReducer<ResponseData>();
+  const { pending, error } = state;
+  const history = useHistory();
+
+  async function handleFormSubmit(fe: FormEvent) {
     fe.preventDefault();
-    console.log(link);
+    dispatch({ type: ActionType.PENDING });
+    try {
+      const res = await axios.post<ResponseData>(IMG_ENDPOINT, { full_url: link });
+      history.push(res.data.location);
+    } catch(e) {
+      dispatch({ type: ActionType.FAILURE, payload: new Error(e.response.data.message) });
+    }
   }
 
+
   return (
-    <div className="container">
-      <form onSubmit={handleFormSubmit}>
-        <div className="form-group">
-          <label htmlFor="imageLink">Image link</label>
-          <input 
-            type="text" 
-            className="form-control" 
-            id="imageLink" 
-            aria-describedby="imageHelp" 
-            value={link}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => { setLink(e.target.value) }}
-          />
-          <small id="imageHelp" className="form-text text-muted">never share your email with anyone else.</small>
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
-    </div>
+    <Container maxWidth="md">
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Box component="form" onSubmit={handleFormSubmit}>
+            <h2 style={{ margin: "0.5rem" }}>Upload your image link</h2>
+            <TextField
+              style={{ margin: "0.5rem" }}
+              error={linkError != null || error != null}
+              disabled={pending}
+              type="text"
+              required
+              fullWidth
+              id="imageLink"
+              label="Image link"
+              value={link}
+              variant="outlined"
+              helperText={error?.message || linkError?.message}
+              onChange={e => {
+                setLink(e.target.value);
+                dispatch({ type: ActionType.RESET });
+              }}
+            />
+            <Box display="flex" justifyContent="center">
+              <Button
+                disabled={linkError != null || error != null || link === "" || pending }
+                size="large"
+                style={{ margin: "0.5rem", width: "20rem" }}
+                type="submit"
+                variant="contained"
+                color="primary"
+              >
+                {pending ? "Uploading...": "Upload"}
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
