891a84176b0845d24b0d648b49607f3d78e3178a
Steven Than <thuyen.than@appfolio.com>
2020-06-02 20:16:48 -0400

    displayed form along with image index

--- app/javascript/App.spec.tsx
+++ app/javascript/App.spec.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React from "react";
+import { mount } from "enzyme";
 import App from "./App";
+import ImageLinkForm from "./components/ImageLinkForm";
+import ImageIndex from "./components/ImageIndex";
+import { act, render } from "@testing-library/react";
+import axios from "axios";
 
+jest.mock("axios");
+const mockAxios = axios as jest.Mocked<typeof axios>;
 
-test("app renders correctly", () => {
-  const { getByText } = render(<App />);
-  expect(getByText(/Upload your image link/i)).toBeInTheDocument();
-})
+test("app renders correctly", async () => {
+  await act(async () => {
+    render(<App />);
+  });
+});
+
+test("contains <ImageLinkForm /> and <ImageIndex />", async () => {
+  mockAxios.get.mockResolvedValueOnce({
+    data: []
+  });
+  await act(async () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find(ImageLinkForm).exists()).toBe(true);
+    expect(wrapper.find(ImageIndex).exists()).toBe(true);
+  });
+});
