8794ea0d13cc7c1a031506f7c064605d76e23eaa
Steven Than <thuyen.than@appfolio.com>
2020-06-07 05:31:10 -0400

    added delete button to ImageView

--- app/javascript/components/ImageView.tsx
+++ app/javascript/components/ImageView.tsx
@@ -1,19 +1,21 @@
 import React, { useEffect, Fragment } from "react";
 import axios from "axios";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useHistory } from "react-router-dom";
 import { Container, Button } from "@material-ui/core";
 
 import { useAsyncReducer, ActionType } from "../hooks";
 import { IMG_ENDPOINT } from "../constants";
 import NotFound from "./NotFound";
 
-interface ResponseData {
+interface GetResponseData {
   full_url: string;
 }
 
 export default function ImageView(): JSX.Element {
   const { imgId } = useParams<{ imgId: string }>();
-  const [state, dispatch] = useAsyncReducer<ResponseData>();
+  const [state, dispatch] = useAsyncReducer<GetResponseData>();
+  const [deleteState, deleteDispatch] = useAsyncReducer();
+  const history = useHistory();
   const { data, error } = state;
   const fullUrl = data?.full_url;
 
@@ -21,13 +23,26 @@ export default function ImageView(): JSX.Element {
     (async () => {
       try {
         dispatch({ type: ActionType.PENDING });
-        const res = await axios.get<ResponseData>(`${IMG_ENDPOINT}/${imgId}`);
+        const res = await axios.get<GetResponseData>(
+          `${IMG_ENDPOINT}/${imgId}`
+        );
         dispatch({ type: ActionType.SUCCESS, payload: res.data });
       } catch (e) {
         dispatch({ type: ActionType.FAILURE, payload: e });
       }
     })();
   }, []);
+
+  async function handleDelete() {
+    deleteDispatch({ type: ActionType.PENDING });
+    try {
+      await axios.delete(`${IMG_ENDPOINT}/${imgId}`);
+    } catch (e) {
+      // ignore failures
+    } finally {
+      history.push("/");
+    }
+  }
 
   if (error?.response?.status === 404) {
     return <NotFound />;
@@ -44,6 +59,15 @@ export default function ImageView(): JSX.Element {
         <Fragment>
           <h2>Viewing {fullUrl}</h2>
           <img style={{ maxWidth: "100%" }} src={fullUrl} />
+          <Button
+            id="imageDelete"
+            disabled={deleteState.pending}
+            variant="contained"
+            color="secondary"
+            onClick={handleDelete}
+          >
+            Delete
+          </Button>
         </Fragment>
       )}
     </Container>
