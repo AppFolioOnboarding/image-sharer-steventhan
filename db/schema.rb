08ee67c9522b2720d08d3cafba4faa7f6995a7e9
Steven Than <thuyen.than@appfolio.com>
2020-06-08 02:54:54 -0400

    added new Feedback model and its db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_033921) do
+ActiveRecord::Schema.define(version: 2020_06_08_044309) do
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "image_links", force: :cascade do |t|
     t.string "full_url"
