bf6bed6f0c89b1cc1dcf025226371bb34db48553
Steven Than <thuyen.than@appfolio.com>
2020-06-08 02:55:53 -0400

    connected frontend with new controller for feedback submission

--- app/javascript/components/FeedbackForm.spec.tsx
+++ app/javascript/components/FeedbackForm.spec.tsx
@@ -1,7 +1,20 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import FeedbackForm from "./FeedbackForm";
-import { Button } from "@material-ui/core";
+import { Button, FormHelperText } from "@material-ui/core";
+import { StaticRouter } from "react-router-dom";
+import { act } from "react-dom/test-utils";
+import axios from "axios";
+
+jest.mock("axios");
+const axiosMock = axios as jest.Mocked<typeof axios>;
+const mockHistoryPush = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
 
 test("renders without crashing", () => {
   shallow(<FeedbackForm />);
@@ -15,4 +28,90 @@ test("has all the fields and button", () => {
   expect(wrapper.find("small").text()).toBe(
     "Copyright: Appfolio Inc. Onboarding"
   );
+});
+
+test("validates fields", () => {
+  const wrapper = mount(
+    <StaticRouter>
+      <FeedbackForm />
+    </StaticRouter>
+  );
+  let button = wrapper.find("button#feedbackSubmit");
+  expect(button.prop("disabled")).toBe(true);
+  const name = wrapper.find("input#name");
+  const comment = wrapper.find("textarea#comment");
+
+  name.simulate("change", { target: { value: "name" } });
+  wrapper.update();
+  button = wrapper.find("button#feedbackSubmit");
+  expect(button.prop("disabled")).toBe(true);
+  name.simulate("change", { target: { value: "" } });
+  comment.simulate("change", { target: { value: "comment" } });
+  wrapper.update();
+  button = wrapper.find("button#feedbackSubmit");
+  expect(button.prop("disabled")).toBe(true);
+  name.simulate("change", { target: { value: "name" } });
+  comment.simulate("change", { target: { value: "comment" } });
+  wrapper.update();
+  button = wrapper.find("button#feedbackSubmit");
+  expect(button.prop("disabled")).toBe(false);
+});
+
+test("submits successfully", async () => {
+  axiosMock.post.mockResolvedValueOnce({});
+  const wrapper = mount(
+    <StaticRouter>
+      <FeedbackForm />
+    </StaticRouter>
+  );
+  const name = wrapper.find("input#name");
+  const comment = wrapper.find("textarea#comment");
+
+  name.simulate("change", { target: { value: "name" } });
+  comment.simulate("change", { target: { value: "comment" } });
+  wrapper.update();
+  const button = wrapper.find("button#feedbackSubmit");
+  expect(button.prop("disabled")).toBe(false);
+  const form = wrapper.find("form");
+
+  await act(async () => {
+    form.simulate("submit");
+  });
+  wrapper.update();
+  expect(wrapper.find("input#name").text()).toBe("");
+  expect(wrapper.find("textarea#comment").text()).toBe("");
+  const helperText = wrapper.find(FormHelperText);
+  expect(helperText.length).toBe(1);
+  expect(helperText.find("p").text()).toBe("Done");
+});
+
+test("submits unsuccessfully", async () => {
+  const message = "Something went wrong";
+  axiosMock.post.mockRejectedValueOnce({
+    response: { data: { message } },
+  });
+  const wrapper = mount(
+    <StaticRouter>
+      <FeedbackForm />
+    </StaticRouter>
+  );
+  const name = wrapper.find("input#name");
+  const comment = wrapper.find("textarea#comment");
+
+  name.simulate("change", { target: { value: "name" } });
+  comment.simulate("change", { target: { value: "comment" } });
+  wrapper.update();
+  const button = wrapper.find("button#feedbackSubmit");
+  expect(button.prop("disabled")).toBe(false);
+  const form = wrapper.find("form");
+
+  await act(async () => {
+    form.simulate("submit");
+  });
+  wrapper.update();
+  expect(wrapper.find("input#name").text()).toBe("");
+  expect(wrapper.find("textarea#comment").text()).toBe("");
+  const helperText = wrapper.find(FormHelperText);
+  expect(helperText.length).toBe(1);
+  expect(helperText.find("p").text()).toBe(message);
 });
--- app/javascript/constants.ts
+++ app/javascript/constants.ts
@@ -1,1 +1,2 @@
 export const IMG_ENDPOINT = "/api/image-links";
+export const FB_ENDPOINT = "/api/feedbacks";
